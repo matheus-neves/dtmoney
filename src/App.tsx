9fc08a3126b0383ed23865802215a10327c71edd
Matheus Neves <matheusneves1989@gmail.com>
2021-03-20 09:59:04 -0300

    feat(Header): Creating component and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global"
-
 
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
