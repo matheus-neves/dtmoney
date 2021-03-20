9b540a5b5bfff7d93da2d8b55c77fb860c34e935
Matheus Neves <matheusneves1989@gmail.com>
2021-03-20 10:29:44 -0300

    feat(TransactionsTable): Creating component and styling

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Summary } from "../Summary"
+import { TransactionsTable } from "../TransactionsTable"
 import { Container } from "./styles"
 
 export const Dashboard = () => {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 }
