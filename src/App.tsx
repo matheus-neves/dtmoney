96b71962abfa1e201a924d51b277372702a0475c
Matheus Neves <matheusneves1989@gmail.com>
2021-03-20 09:34:00 -0300

    feat: Creating global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global"
+
+
 export const App = () => {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
